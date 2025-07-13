01475a0393bb9f20fe250f3958906da98e6f10a8
Gokul Shaji <gokulshaji2005@gmail.com>
2025-07-13 20:08:13 +0530

    firstNnumbers

---
+++ basics/firstNnumbers.java
@@ -0,0 +1,17 @@
+package basics;
+import java.util.Scanner;
+public class firstNnumbers {
+    public static void main(String args[]){
+      Scanner sc=new Scanner(System.in);
+      int limit;
+      int sum=0;
+      System.out.println("Enter the limit:");
+      limit=sc.nextInt();
+
+      for(int i=1;i<=limit;i++){
+        System.out.println(i);
+        sum=sum+i;
+      }
+      System.out.println("The sum of first"+" "+limit+" numbers is"+" "+sum);
+    }
+}
