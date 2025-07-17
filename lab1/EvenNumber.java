460ed6c359dd4af0b59f6a1e4e3ecf0aa2aec5f8
Gokul Shaji <gokulshaji2005@gmail.com>
2025-07-17 21:34:11 +0530

    Added lab

---
+++ lab1/EvenNumber.java
@@ -0,0 +1,17 @@
+package lab1;
+
+public class EvenNumber{
+public static void main (String args []){
+    int i=2;
+    do{
+       
+        System.out.println(i);
+        i+=2;
+    }while(i<=20);
+}  
+
+}
+/*
+ * author:Gokul Shaji
+ * Date:17-7-2025
+ */
