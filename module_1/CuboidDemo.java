4beb318567f72fa26804c0812ed7ada2234c9adf
Gokul Shaji <gokulshaji2005@gmail.com>
2025-07-22 22:13:39 +0530

    added two classes

---
+++ module_1/CuboidDemo.java
@@ -0,0 +1,20 @@
+package module_1;
+import java.util.Scanner;
+ 
+public class CuboidDemo {
+	public static void main(String[] args)
+	{
+				System.out.println("Please enter the length,breadth,height of the cuboid:");
+				Scanner sc = new Scanner(System.in);
+				double length = sc.nextDouble();
+				double breadth= sc.nextDouble();
+				double height = sc.nextDouble();
+				Cuboid mybox = new Cuboid();
+				mybox.setDimension(length,breadth,height);
+				double volume =mybox.calculateVolume();
+				System.out.println("Volume of the cuboid with dimensions: "+length+"* "
+				+breadth+" *"+height+" is :"+volume);
+				double surfaceArea = mybox.calculateSurfaceArea();
+		        System.out.println("Surface Area of the box is: " + surfaceArea);
+			}
+	}
